16d58103eda969be3af79e8e960394a5a760a059
Neovitalism <neovitalism@gmail.com>
2023-04-30 13:53:33 -0400

    to mini hover because I don't use components until last second

--- src/main/java/me/neovitalism/neoapi/helpers/ItemStackHelper.java
+++ src/main/java/me/neovitalism/neoapi/helpers/ItemStackHelper.java
@@ -2,8 +2,11 @@ package me.neovitalism.neoapi.helpers;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+
+import java.util.Locale;
 
 public class ItemStackHelper {
     public static Identifier getIdentifier(Item item) {
@@ -12,5 +15,23 @@ public class ItemStackHelper {
 
     public static Identifier getIdentifier(ItemStack item) {
         return getIdentifier(item.getItem());
+    }
+
+    public static String toMiniItemHover(ItemStack item) {
+        NbtCompound itemNBT = item.getNbt();
+        if(itemNBT != null) {
+            return "<hover:show_item:"
+                    + item.getItem().getName().getString()
+                    .toLowerCase(Locale.ENGLISH)
+                    .replace(" ", "_") +
+                    ":" + item.getCount() + ":\"" +
+                    itemNBT + "\">";
+        } else {
+            return "<hover:show_item:"
+                    + item.getItem().getName().getString()
+                    .toLowerCase(Locale.ENGLISH)
+                    .replace(" ", "_")
+                    + ":" + item.getCount() + ">";
+        }
     }
 }
