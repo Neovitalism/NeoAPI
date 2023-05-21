2f827343067d64b02b16525884646c00bb530b02
Neovitalism <neovitalism@gmail.com>
2023-05-21 15:45:11 -0400

    Update adventure and move LocationUtil into the API

---
+++ src/main/java/me/neovitalism/neoapi/utils/LocationUtil.java
@@ -0,0 +1,14 @@
+package me.neovitalism.neoapi.utils;
+
+import me.neovitalism.neoapi.objects.Location;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.Vec3d;
+
+public class LocationUtil {
+    public static Location getLookingAt(ServerPlayerEntity player) {
+        HitResult hitResult = player.raycast(500, 0, false);
+        Vec3d target = hitResult.getPos();
+        return new Location(player.getWorld(), target.x, target.y, target.z);
+    }
+}
