33595a12daa63e8b8ae715af893b0d384fae7ac5
Neovitalism <neovitalism@gmail.com>
2023-06-05 04:46:52 -0400

    ItemStackHelper.fromConfig()

--- src/main/java/me/neovitalism/neoapi/helpers/ItemStackHelper.java
+++ src/main/java/me/neovitalism/neoapi/helpers/ItemStackHelper.java
@@ -1,11 +1,20 @@
 package me.neovitalism.neoapi.helpers;
 
+import me.neovitalism.neoapi.modloading.config.Configuration;
+import me.neovitalism.neoapi.utils.ColorUtil;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.jetbrains.annotations.Nullable;
 
+import java.awt.*;
+import java.util.List;
 import java.util.Locale;
 
 public class ItemStackHelper {
@@ -33,5 +42,73 @@ public class ItemStackHelper {
                     .replace(" ", "_")
                     + ":" + item.getCount() + ">";
         }
+    }
+
+    public static ItemStack fromConfig(Configuration itemConfig, int amount) {
+        Item item = Registry.ITEM.get(Identifier.tryParse(itemConfig.getString("Material")));
+        if(item != Items.AIR) {
+            ItemStack itemStack = new ItemStack(item);
+            String name = itemConfig.getString("Name");
+            if(name != null) itemStack.setCustomName(ColorUtil.parseColourToText(name));
+            List<String> lore = itemConfig.getStringList("Lore");
+            StringBuilder loreBuilder = new StringBuilder();
+            int i = 0;
+            for(String line : lore) {
+                if(i != 0) loreBuilder.append("\n");
+                loreBuilder.append(line);
+                i++;
+            }
+            if(!loreBuilder.isEmpty()) setLore(itemStack, ColorUtil.parseColourToText(loreBuilder.toString()));
+            itemStack.setCount((amount == -1) ? itemConfig.getInt("Amount", 1) : amount);
+            List<String> enchants = itemConfig.getStringList("Enchants");
+            for(String enchant : enchants) {
+                String[] enchantParsed = enchant.split(" ");
+                if(enchantParsed.length != 2) continue;
+                Identifier enchantID = Identifier.tryParse(enchantParsed[0]);
+                Enchantment enchantment = Registry.ENCHANTMENT.get(enchantID);
+                if(enchantment == null) continue;
+                int level;
+                try {
+                    level = Integer.parseInt(enchantParsed[1]);
+                } catch (NumberFormatException e) {
+                    continue;
+                }
+                itemStack.addEnchantment(enchantment, level);
+            }
+            Configuration nbtConfig = itemConfig.getSection("NBT");
+            if(nbtConfig != null) {
+                NbtCompound itemStackNBT = itemStack.getOrCreateNbt();
+                for(String nbtKey : nbtConfig.getKeys()) {
+                    Object nbtValue = nbtConfig.get(nbtKey);
+                    if(nbtValue instanceof Byte) {
+                        itemStackNBT.putByte(nbtKey, (Byte) nbtValue);
+                    } else if(nbtValue instanceof Integer) {
+                        itemStackNBT.putInt(nbtKey, (Integer) nbtValue);
+                    } else if(nbtValue instanceof Long) {
+                        itemStackNBT.putLong(nbtKey, (Long) nbtValue);
+                    } else if(nbtValue instanceof Float) {
+                        itemStackNBT.putFloat(nbtKey, (Float) nbtValue);
+                    } else if(nbtValue instanceof Double) {
+                        itemStackNBT.putDouble(nbtKey, (Double) nbtValue);
+                    } else if(nbtValue instanceof Boolean) {
+                        itemStackNBT.putBoolean(nbtKey, (Boolean) nbtValue);
+                    } else if(nbtValue instanceof String) {
+                        itemStackNBT.putString(nbtKey, (String) nbtValue);
+                    }
+                }
+                itemStack.setNbt(itemStackNBT);
+            }
+            return itemStack;
+        } else return null;
+    }
+
+    public static ItemStack setLore(ItemStack item, Text lore) {
+        NbtCompound nbtCompound = item.getOrCreateSubNbt("display");
+        if (lore != null) {
+            nbtCompound.putString("Lore", Text.Serializer.toJson(lore));
+        } else {
+            nbtCompound.remove("Lore");
+        }
+        return item;
     }
 }
---
+++ src/main/java/me/neovitalism/neoapi/protection/DuplicationProtection.java
@@ -0,0 +1,11 @@
+package me.neovitalism.neoapi.protection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class DuplicationProtection {
+    private static final Map<UUID, Integer> existingItems = new HashMap<>();
+
+    // ToDo: Well, this.
+}
