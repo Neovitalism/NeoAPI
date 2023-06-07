5165b0118d2c7e59f30b144d013506c1dbbc977c
Neovitalism <neovitalism@gmail.com>
2023-06-07 07:35:55 -0400

    LocationUtil.getLookingAtEntity()

--- src/main/java/me/neovitalism/neoapi/utils/LocationUtil.java
+++ src/main/java/me/neovitalism/neoapi/utils/LocationUtil.java
@@ -1,7 +1,9 @@
 package me.neovitalism.neoapi.utils;
 
 import me.neovitalism.neoapi.objects.Location;
+import net.minecraft.entity.Entity;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
 
@@ -10,5 +12,12 @@ public class LocationUtil {
         HitResult hitResult = player.raycast(500, 0, false);
         Vec3d target = hitResult.getPos();
         return new Location(player.getWorld(), target.x, target.y, target.z);
+    }
+
+    public static Entity getLookingAtEntity(ServerPlayerEntity player, int maxDistance) {
+        HitResult hitResult = player.raycast(maxDistance, 0, false);
+        if(hitResult.getType() == HitResult.Type.ENTITY) {
+            return ((EntityHitResult)hitResult).getEntity();
+        } else return null;
     }
 }
