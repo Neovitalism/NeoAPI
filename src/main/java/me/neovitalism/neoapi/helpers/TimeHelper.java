eb5a9102198dd4ceb73b542737148d3d100e6fbe
Neovitalism <neovitalism@gmail.com>
2023-06-14 16:07:13 -0400

    TimeHelper.addReplacements

---
+++ src/main/java/me/neovitalism/neoapi/helpers/TimeHelper.java
@@ -0,0 +1,20 @@
+package me.neovitalism.neoapi.helpers;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class TimeHelper {
+    public static void addReplacements(Map<String, String> replacements, long milliseconds) {
+        long secondsTotal = TimeUnit.MILLISECONDS.toSeconds(milliseconds);
+        long days = TimeUnit.SECONDS.toDays(secondsTotal);
+        long hoursNoDays = TimeUnit.SECONDS.toHours(secondsTotal);
+        long hours = TimeUnit.DAYS.toHours(days) - hoursNoDays;
+        long minutes = TimeUnit.SECONDS.toMinutes(secondsTotal) - TimeUnit.HOURS.toMinutes(hoursNoDays);
+        long seconds = secondsTotal - TimeUnit.HOURS.toSeconds(hoursNoDays) - TimeUnit.MINUTES.toSeconds(minutes);
+        replacements.put("{days}", String.valueOf(days));
+        replacements.put("{hours-no-days}", String.valueOf(hoursNoDays));
+        replacements.put("{hours}", String.valueOf(hours));
+        replacements.put("{minutes}", String.valueOf(minutes));
+        replacements.put("{seconds}", String.valueOf(seconds));
+    }
+}
